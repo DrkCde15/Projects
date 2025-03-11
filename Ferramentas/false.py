c3974c06fd4557a705b44a731f0f2ac071b23bbb
DrkCde15 <jcesarsantana215@gmail.com>
2025-03-11 12:35:40 -0300

    3.0

--- Ferramentas/false.py
+++ Ferramentas/false.py
@@ -2,6 +2,7 @@ import random
 from datetime import datetime, timedelta
 
 def luhn(digits):
+    """Calcula o dígito verificador pelo algoritmo de Luhn"""
     sum_ = 0
     reverse_digits = digits[::-1]
     for i, d in enumerate(reverse_digits):
@@ -14,12 +15,17 @@ def luhn(digits):
     return (10 - (sum_ % 10)) % 10
 
 def gerar_numero_cartao():
+    # Gerar os primeiros 15 dígitos do cartão de crédito aleatoriamente
     prefixo_cartao = [str(random.randint(0, 9)) for _ in range(15)]
+    # Calcular o dígito verificador com base nos 15 dígitos gerados
     digito_verificador = luhn(prefixo_cartao)
+    # Adicionar o dígito verificador ao número do cartão
     prefixo_cartao.append(str(digito_verificador))
+    # Retornar o cartão no formato correto
     return ''.join(prefixo_cartao)
 
 def formata_cartao(cartao):
+    # Formatando o número do cartão no formato XXXX XXXX XXXX XXXX
     return f"{cartao[:4]} {cartao[4:8]} {cartao[8:12]} {cartao[12:16]}"
 
 def gera_cartoes_em_lote(quantidade):
@@ -64,25 +70,60 @@ def gerar_telefone(estado):
     return f"({random.choice(ddd)}) {random.randint(1000, 9999)}-{random.randint(1000, 9999)}"
 
 def gerar_dados():
-    nomes = [
-        "Rosa Pietra Sophia Fogaca", "João Silva", "Maria Oliveira", "Carlos Souza", "Ana Beatriz Martins", "Lucas Almeida",
-        "Fernanda Pereira", "Paulo Sérgio Costa", "Mariana Gonçalves", "Gustavo Fernandes", "Clara Mendes", "Ricardo Lima",
-        "Isabella Ribeiro", "José Barbosa", "Camila Araújo", "Felipe Duarte", "Sofia Carvalho", "Pedro Gonçalves", "Laura Nunes",
-        "Thiago Matos", "Gabriela Rocha", "Daniel Moreira", "Julia Ferreira", "Vinicius Alves", "Lara Santos", "Matheus Ribeiro",
-        "Luana Souza", "Henrique Teixeira", "Livia Andrade", "Rodrigo Costa", "Yasmin Almeida", "Eduardo Lima", "Alice Martins",
-        "Rafael Pereira", "Helena Correia", "André Silva", "Sophia Machado", "Diego Mendes", "Valentina Barbosa", "Gabriel Alves",
-        "Carolina Rocha", "Bruno Oliveira", "Beatriz Nunes", "Leonardo Azevedo", "Cecilia Fernandes", "Marcelo Almeida", "Clara Gomes",
-        "Antonio Melo", "Lorena Vieira", "Miguel Santana", "Renata Cardoso", "Guilherme Ferreira", "Marina Moreira", "Caio Souza",
-        "Amanda Costa", "Ruan Ribeiro", "Flávia Silva", "Arthur Rocha", "Elisa Nunes", "Junior Santos", "Nicole Ferreira",
-        "Rafaela Mendes", "Heitor Martins", "Marcia Souza", "Vitor Lima", "Tainá Araújo", "Igor Alves", "Letícia Pereira",
-        "Joana Andrade", "Hugo Fernandes", "Aline Duarte", "Otávio Ribeiro", "Cristina Nogueira", "Leandro Batista", "Larissa Teixeira",
-        "Pablo Matos", "Emilly Carvalho", "Daniela Souza", "Fábio Gomes", "Bruna Martins", "Sandro Costa", "Mirela Ferreira",
-        "Robson Lima", "Lorraine Vieira", "Patrick Oliveira", "Isabel Fernandes", "Diogo Gonçalves", "Melina Rocha", "Roberto Santos",
-        "Stephanie Souza", "Alan Nunes", "Clarice Matos", "Wagner Almeida", "Geovana Pereira", "Eduarda Alves", "Otávio Nunes",
-        "Samuel Silva", "Jéssica Almeida", "Enzo Costa", "Bianca Ribeiro", "Marcela Souza", "Filipe Cardoso", "Sara Martins",
-        "Victor Araújo", "Amanda Gonçalves", "Manuela Lima", "Murilo Santos", "Raquel Mendes", "Danilo Costa", "Lívia Fernandes"
-    ]
-    sexos = ["Feminino", "Masculino"]
+    # Listas de nomes masculinos e femininos
+    nomes_masculinos = ["João Silva", "Carlos Souza", "Lucas Almeida", "Paulo Sérgio Costa", "Gustavo Fernandes", 
+    "Felipe Duarte", "Pedro Gonçalves", "José Barbosa", "Thiago Matos", "Daniel Moreira", 
+    "Vinicius Alves", "Matheus Ribeiro", "Henrique Teixeira", "Rodrigo Costa", "Eduardo Lima", 
+    "Rafael Pereira", "André Silva", "Diego Mendes", "Gabriel Alves", "Bruno Oliveira", 
+    "Leonardo Azevedo", "Marcelo Almeida", "Antonio Melo", "Miguel Santana", "Guilherme Ferreira", 
+    "Caio Souza", "Ruan Ribeiro", "Arthur Rocha", "Junior Santos", "Heitor Martins", "Vitor Lima", 
+    "Igor Alves", "Hugo Fernandes", "Otávio Ribeiro", "Leandro Batista", "Pablo Matos", "Fábio Gomes", 
+    "Robson Lima", "Patrick Oliveira", "Diogo Gonçalves", "Roberto Santos", "Alan Nunes", "Wagner Almeida", 
+    "Otávio Nunes", "Samuel Silva", "Enzo Costa", "Filipe Cardoso", "Victor Araújo", "Murilo Santos", 
+    "Danilo Costa", "Alexandre Martins", "Cláudio Rocha", "Francisco Pereira", "Ricardo Teixeira", 
+    "Fabiano Oliveira", "Leandro Nunes", "Douglas Lima", "Marcos Silva", "Eduardo Rocha", "Alex Silva", 
+    "Lucas Teixeira", "Thiago Almeida", "Gustavo Costa", "Vinícius Ribeiro", "Guilherme Santos", 
+    "Robson Rocha", "Felipe Souza", "Afonso Martins", "William Almeida", "Daniela Costa", "Marcelo Santos", 
+    "Fernando Azevedo", "Arthur Teixeira", "Tiago Lima", "Renato Ribeiro", "Claudio Fernandes", "Lucas Mendes", 
+    "Gabriel Cardoso", "Victor Pereira", "Maurício Silva", "Vítor Souza", "Carlos Martins", "André Barbosa", 
+    "Felipe Nunes", "Mateus Souza", "David Lima", "Thiago Ribeiro", "Pedro Martins", "Leandro Souza", 
+    "Vanderlei Silva", "Wagner Santos", "Paulo Azevedo", "Hugo Almeida", "Luiz Costa", "Alexandre Azevedo", 
+    "Ricardo Barbosa", "Fábio Ribeiro", "Vinicius Nunes", "Carlos Azevedo", "Thiago Gomes", "Davi Lima", 
+    "Roberto Silva", "Pablo Costa", "Samuel Pereira", "Rafael Nunes", "Renato Santos", "André Costa", 
+    "Eduardo Ribeiro", "Marcelo Lima", "Bruno Azevedo", "Diego Teixeira", "Maurício Costa", "Gustavo Almeida", 
+    "Ricardo Silva", "Vítor Ribeiro", "Marcelo Nunes", "Alan Rocha", "Robson Ribeiro", "Victor Nunes", 
+    "Hugo Costa", "Marcos Ribeiro", "Vinicius Gomes", "Afonso Silva", "Felipe Pereira", "Leonardo Ribeiro", 
+    "Carlos Souza", "Júlio Teixeira", "Gabriel Rocha", "Arthur Silva", "Felipe Gomes", "Paulo Ribeiro", 
+    "Rafael Costa", "Tiago Santos", "Alex Ribeiro", "Bruno Mendes", "Carlos Teixeira", "Vítor Gomes", 
+    "Samuel Nunes", "Leandro Pereira", "Lucas Gomes", "Danilo Ribeiro", "Felipe Fernandes", "Ricardo Lima", 
+    "Pedro Souza", "Gilberto Nunes", "Douglas Ribeiro", "Mateus Ribeiro", "Rafael Gomes", "Alan Santos"
+]
+    
+    nomes_femininos = ["Rosa Pietra Sophia Fogaca", "Maria Oliveira", "Ana Beatriz Martins", 
+        "Fernanda Pereira", "Mariana Gonçalves", "Clara Mendes", "Isabella Ribeiro", "Camila Araújo", "Sofia Carvalho", "Laura Nunes",
+        "Gabriela Rocha", "Julia Ferreira", "Lara Santos", "Luana Souza", "Livia Andrade", "Yasmin Almeida", "Alice Martins",
+        "Helena Correia", "Sophia Machado", "Valentina Barbosa", "Carolina Rocha", "Beatriz Nunes", "Cecilia Fernandes", "Clara Gomes",
+        "Lorena Vieira", "Renata Cardoso", "Marina Moreira", "Amanda Costa", "Flávia Silva", "Elisa Nunes", "Nicole Ferreira",
+        "Rafaela Mendes", "Marcia Souza", "Tainá Araújo", "Letícia Pereira", "Joana Andrade", "Aline Duarte", "Cristina Nogueira", "Larissa Teixeira",
+        "Emilly Carvalho", "Daniela Souza", "Bruna Martins", "Sandra Costa", "Mirela Ferreira",
+        "Lorraine Vieira", "Isabel Fernandes", "Melina Rocha",
+        "Stephanie Souza", "Clarice Matos", "Geovana Pereira", "Eduarda Alves",
+        "Jéssica Almeida", "Bianca Ribeiro", "Marcela Souza", "Sara Martins",
+        "Amanda Gonçalves", "Manuela Lima", "Raquel Mendes", "Lívia Fernandes"]
+
+    # Escolher aleatoriamente entre masculino e feminino
+    sexo = random.choice(["Masculino", "Feminino"])
+    
+    # Escolher um nome correspondente ao sexo
+    if sexo == "Masculino":
+        nome = random.choice(nomes_masculinos)
+    else:
+        nome = random.choice(nomes_femininos)
+
+    # Gerar email a partir do nome
+    email = nome.lower().replace(' ', '.') + "@example.com"
+
+    # Outros dados
     signos = ["Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem", "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"]
     estados = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
     cidades = {
@@ -114,8 +155,7 @@ def gerar_dados():
         "SE": ["Aracaju", "Nossa Senhora do Socorro"],
         "TO": ["Palmas", "Araguaína"]
     }
-    bairros = [
-        "Centro", "Jardim das Flores", "Vila Nova", "Maria Rachel", "Jardim América", "Vila Mariana", "Bela Vista",
+    bairros = [ "Centro", "Jardim das Flores", "Vila Nova", "Maria Rachel", "Jardim América", "Vila Mariana", "Bela Vista",
         "Consolação", "Moema", "Liberdade", "Pinheiros", "Santana", "Tatuapé", "Jardim Paulista", "Jardim Europa",
         "Lapa", "Itaim Bibi", "Morumbi", "Campo Belo", "Jabaquara", "Brooklin", "Perdizes", "Vila Olímpia",
         "Aclimação", "Cambuci", "Vila Madalena", "Chácara Santo Antônio", "Parque do Carmo", "Sapopemba", "Brás",
@@ -130,10 +170,9 @@ def gerar_dados():
         "Vila Clementino", "Pacaembu", "Chácara Klabin", "Parque Bristol", "Vila Diva", "Parque São Rafael",
         "São Domingos", "Vila Carmosina", "Vila Gomes Cardim", "Jardim Vila Galvão", "Jardim Alvorada", "Jardim Esmeralda",
         "Vila Matilde", "Parque Santo Antônio", "Vila Moraes", "Vila Invernada", "Vila Califórnia", "Jardim Brasil",
-        "Jardim Aricanduva", "Parque São Jorge", "Jardim Morumbi", "Chácara Flora", "Vila Ipojuca", "Jardim Olinda"
-    ]
-    enderecos = [
-        "Rua João Patrício de Lima", "Avenida Paulista", "Rua das Flores", "Praça da Sé", "Rua Augusta", "Rua Oscar Freire",
+        "Jardim Aricanduva", "Parque São Jorge", "Jardim Morumbi", "Chácara Flora", "Vila Ipojuca", "Jardim Olinda"]
+    
+    enderecos = ["Rua João Patrício de Lima", "Avenida Paulista", "Rua das Flores", "Praça da Sé", "Rua Augusta", "Rua Oscar Freire",
         "Avenida Brigadeiro Luís Antônio", "Rua Bela Cintra", "Rua da Consolação", "Rua Vergueiro", "Avenida Faria Lima",
         "Avenida Rebouças", "Rua Teodoro Sampaio", "Rua dos Pinheiros", "Avenida Nove de Julho", "Rua Joaquim Floriano",
         "Avenida Juscelino Kubitschek", "Rua Bandeira Paulista", "Avenida Ibirapuera", "Rua Domingos de Morais",
@@ -153,8 +192,8 @@ def gerar_dados():
         "Rua Olívia Guedes Penteado", "Rua Alagoas", "Rua Capote Valente", "Rua Doutor Homem de Mello", "Rua Diana",
         "Rua Votupoca", "Avenida das Nações Unidas", "Rua Pedroso Alvarenga", "Rua Leopoldo Couto de Magalhães",
         "Rua João Cachoeira", "Rua Vinte e Cinco de Março", "Rua do Carmo", "Rua Piratininga", "Rua Alfredo Maia",
-        "Rua Humberto I", "Rua José Maria Lisboa", "Rua Dr. Rafael de Barros", "Rua Rafael de Barros"
-    ]
+        "Rua Humberto I", "Rua José Maria Lisboa", "Rua Dr. Rafael de Barros", "Rua Rafael de Barros"]
+    
     tipo_sanguineo = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]
 
     estado = random.choice(estados)
@@ -163,31 +202,25 @@ def gerar_dados():
     endereco = random.choice(enderecos)
     cep = f"{random.randint(10000, 99999)}-{random.randint(100, 999)}"
 
-    sexo = random.choice(sexos)
-    if sexo == "Masculino":
-        altura = f"1,{random.randint(50, 90)}"
-    else:
-        altura = f"1,{random.randint(50, 70)}"
-
     return {
-        "Nome": random.choice(nomes),
-        "CPF": gerar_cpf(),
-        "RG": gerar_rg(),
-        "Data de Nascimento": gerar_data_nascimento(),
-        "Sexo": sexo,
-        "Signo": random.choice(signos),
-        "Mãe": f"Rosângela {random.choice(['Antônia', 'Maria', 'Clara'])}",
-        "Pai": f"Marcelo {random.choice(['Pedro', 'João', 'Carlos'])} Silva",  # Corrigido para incluir espaço
-        "Email": f"{random.choice(nomes).lower().replace(' ', '.')}@gmail.com",
-        "CEP": cep,
-        "Endereço": endereco,
-        "Bairro": bairro,
-        "Cidade": cidade,
-        "Estado": estado,
-        "Telefone Fixo": gerar_telefone(estado),
-        "Celular": gerar_telefone(estado),
-        "Altura": altura,
-        "Tipo Sanguíneo": random.choice(tipo_sanguineo)
+        "nome": nome,
+        "cpf": gerar_cpf(),
+        "rg": gerar_rg(),
+        "data_nasc": gerar_data_nascimento(),
+        "sexo": sexo,
+        "signo": random.choice(signos),
+        "mae": f"Josefa {random.choice(['Antônia', 'Maria', 'Clara'])}",
+        "pai": f"Marcelo {random.choice(['Pedro', 'João', 'Carlos'])} Silva",
+        "email": email,
+        "cep": cep,
+        "endereço": endereco,
+        "bairro": bairro,
+        "cidade": cidade,
+        "estado": estado,
+        "telefone_fixo": gerar_telefone(estado),
+        "celular": gerar_telefone(estado),
+        "altura": f"1,{random.randint(50, 99)}",
+        "tipo_sanguíneo": random.choice(tipo_sanguineo)
     }
 
 def verificar_bin(bin):
@@ -209,7 +242,7 @@ def testar_luhn(numero_cartao):
         for d in even_digits:
             checksum += sum(digits_of(d*2))
         return checksum % 10
-
+    
     valido = luhn_checksum(int(numero_cartao)) == 0
     print(f"O número do cartão {numero_cartao} é {'válido' if valido else 'inválido'}.")
 
@@ -223,7 +256,7 @@ def menu():
         print("5. Gerar Cartões em Lote")
         print("6. Sair")
         opcao = input("Opção: ")
-
+        
         if opcao == "1":
             cartao = gerar_numero_cartao()
             print(f"Cartão gerado: {formata_cartao(cartao)}")
@@ -249,7 +282,7 @@ def menu():
             for cartao in cartoes:
                 print(cartao)
         elif opcao == "6":
-            print("Saindo, Obrigado por usar o programa!")
+            print("Saindo...")
             break
         else:
             print("Opção inválida. Tente novamente.")
