e7300bfc22526831b62e2cc8b91c2128ab1b329b
DrkCde15 <jcesarsantana215@gmail.com>
2025-03-20 14:26:39 -0300

    project_card 2.0

--- happy_nation/project_cards/myproject/cartao/forms.py
+++ happy_nation/project_cards/myproject/cartao/forms.py
@@ -4,4 +4,4 @@ from .models import Cartao
 class CartaoForm(forms.ModelForm):
     class Meta:
         model = Cartao
-        fields = ['nome_titular', 'numero_cartao', 'data_validade', 'codigo_seguranca']
+        fields = ['nome_titular', 'numero_cartao', 'data_validade', 'codigo_seguranca', 'senha_cartao']
--- happy_nation/project_cards/myproject/cartao/models.py
+++ happy_nation/project_cards/myproject/cartao/models.py
@@ -5,6 +5,7 @@ class Cartao(models.Model):
     numero_cartao = models.CharField(max_length=16)
     data_validade = models.CharField(max_length=5)
     codigo_seguranca = models.CharField(max_length=3)
+    senha_cartao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome_titular
