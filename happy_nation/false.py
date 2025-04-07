16055e6b6ec8f21934a184a92952e5bc68f8407f
DrkCde15 <jcesarsantana215@gmail.com>
2025-04-07 11:03:13 -0300

    gerar_dados upgrade

--- happy_nation/false.py
+++ happy_nation/false.py
@@ -110,7 +110,7 @@ def gerar_dados():
         "Stephanie Souza", "Clarice Matos", "Geovana Pereira", "Eduarda Alves",
         "Jéssica Almeida", "Bianca Ribeiro", "Marcela Souza", "Sara Martins",
         "Amanda Gonçalves", "Manuela Lima", "Raquel Mendes", "Lívia Fernandes"
-        ]
+    ]
 
     # Escolher aleatoriamente entre masculino e feminino
     sexo = random.choice(["Masculino", "Feminino"])
@@ -156,53 +156,129 @@ def gerar_dados():
         "SE": ["Aracaju", "Nossa Senhora do Socorro"],
         "TO": ["Palmas", "Araguaína"]
     }
-    bairros = [ "Centro", "Jardim das Flores", "Vila Nova", "Maria Rachel", "Jardim América", "Vila Mariana", "Bela Vista",
-        "Consolação", "Moema", "Liberdade", "Pinheiros", "Santana", "Tatuapé", "Jardim Paulista", "Jardim Europa",
-        "Lapa", "Itaim Bibi", "Morumbi", "Campo Belo", "Jabaquara", "Brooklin", "Perdizes", "Vila Olímpia",
-        "Aclimação", "Cambuci", "Vila Madalena", "Chácara Santo Antônio", "Parque do Carmo", "Sapopemba", "Brás",
-        "Mooca", "Butantã", "Ipiranga", "São Mateus", "Vila Prudente", "Vila Leopoldina", "Parque São Lucas", "Artur Alvim",
-        "São Miguel Paulista", "Jardim São Paulo", "Parque Peruche", "Vila Matilde", "Cidade Ademar", "Parque da Mooca",
-        "Barra Funda", "Vila Formosa", "Jardim São Bento", "Jardim Vila Mariana", "Capão Redondo", "Cidade Tiradentes",
-        "Pirituba", "Freguesia do Ó", "Jaraguá", "Vila Sônia", "Cangaíba", "Jardim Bonfiglioli", "Ermelino Matarazzo",
-        "Grajaú", "Interlagos", "Pedreira", "Sacomã", "Tremembé", "Jardim Ângela", "Itaquera", "Vila Carrão", "Vila Ema",
-        "Cidade Patriarca", "Jardim Anália Franco", "Jardim Panorama", "Água Branca", "Rio Pequeno", "Raposo Tavares",
-        "Vila Brasilândia", "Ponte Rasa", "Jardim Helena", "Jardim São Luís", "Vila Curuçá", "Vila Medeiros",
-        "Jardim Avelino", "Parque Novo Mundo", "Cidade Líder", "Jardim Marajoara", "Cidade Dutra", "Vila Guarani",
-        "Vila Clementino", "Pacaembu", "Chácara Klabin", "Parque Bristol", "Vila Diva", "Parque São Rafael",
-        "São Domingos", "Vila Carmosina", "Vila Gomes Cardim", "Jardim Vila Galvão", "Jardim Alvorada", "Jardim Esmeralda",
-        "Vila Matilde", "Parque Santo Antônio", "Vila Moraes", "Vila Invernada", "Vila Califórnia", "Jardim Brasil",
-        "Jardim Aricanduva", "Parque São Jorge", "Jardim Morumbi", "Chácara Flora", "Vila Ipojuca", "Jardim Olinda"
-        ]
+    # Dicionários de bairros por estado
+    bairros_por_estado = {
+        "RJ": ["Copacabana", "Ipanema", "Leblon", "Barra da Tijuca", "Tijuca", 
+               "Botafogo", "Flamengo", "Laranjeiras", "Santa Teresa", "Centro",
+               "Vila Isabel", "Grajaú", "Jacarepaguá", "Recreio dos Bandeirantes",
+               "São Conrado", "Gávea", "Jardim Botânico", "Catete", "Cosme Velho",
+               "Leme", "Urca", "Rocinha", "Vidigal", "Méier", "Madureira"],
+        
+        "SP": ["Moema", "Pinheiros", "Vila Madalena", "Itaim Bibi", "Jardins",
+              "Morumbi", "Brooklin", "Liberdade", "Bela Vista", "Consolação",
+              "Perdizes", "Vila Olímpia", "Santo Amaro", "Lapa", "Tatuapé",
+              "Vila Mariana", "Higienópolis", "Campo Belo", "Paraíso", "Butantã",
+              "Santana", "Vila Leopoldina", "Alphaville", "Interlagos", "Ipiranga"],
+        
+        "MG": ["Savassi", "Funcionários", "Lourdes", "Sion", "Belo Horizonte",
+              "Santa Tereza", "Pampulha", "Cidade Nova", "Barro Preto", "Nova Lima",
+              "Contagem", "Betim", "Venda Nova", "Santa Efigênia", "Horto"],
+        
+        "BA": ["Barra", "Ondina", "Rio Vermelho", "Pituba", "Brotas",
+              "Graça", "Caminho das Árvores", "Horto Florestal", "Stella Maris",
+              "Itaigara", "Cabula", "Liberdade", "Pelourinho", "Comércio", "Federação"],
+        
+        "RS": ["Moinhos de Vento", "Bela Vista", "Petrópolis", "Boa Vista", "Menino Deus",
+              "Jardim Botânico", "Tristeza", "Cidade Baixa", "Centro Histórico", "Santana",
+              "Partenon", "Vila Assunção", "Auxiliadora", "Floresta", "Independência"],
+        
+        "PR": ["Batel", "Água Verde", "Bigorrilho", "Centro Cívico", "Cabral",
+              "Mercês", "Jardim Social", "Cristo Rei", "Portão", "Santa Felicidade",
+              "Vila Izabel", "Hugo Lange", "Alto da Glória", "Seminário", "Juvevê"],
+        
+        "PE": ["Boa Viagem", "Pina", "Brasília Teimosa", "Casa Forte", "Graças",
+              "Espinheiro", "Parnamirim", "Poço da Panela", "Santana", "Apipucos",
+              "Madalena", "Torre", "Derby", "Ilha do Leite", "Santo Antônio"],
+        
+        "CE": ["Meireles", "Aldeota", "Praia de Iracema", "Dionísio Torres", "Varjota",
+              "Benfica", "Fátima", "Parquelândia", "Montese", "Jardim América",
+              "Cocó", "Papicu", "Cambeba", "Edson Queiroz", "Sapiranga"],
+        
+        "DF": ["Asa Sul", "Asa Norte", "Lago Sul", "Lago Norte", "Sudoeste",
+              "Cruzeiro", "Guará", "Taguatinga", "Águas Claras", "Sobradinho",
+              "Planaltina", "Gama", "Brazlândia", "Núcleo Bandeirante", "Park Way"]
+    }
     
-    enderecos = ["Rua João Patrício de Lima", "Avenida Paulista", "Rua das Flores", "Praça da Sé", "Rua Augusta", "Rua Oscar Freire",
-        "Avenida Brigadeiro Luís Antônio", "Rua Bela Cintra", "Rua da Consolação", "Rua Vergueiro", "Avenida Faria Lima",
-        "Avenida Rebouças", "Rua Teodoro Sampaio", "Rua dos Pinheiros", "Avenida Nove de Julho", "Rua Joaquim Floriano",
-        "Avenida Juscelino Kubitschek", "Rua Bandeira Paulista", "Avenida Ibirapuera", "Rua Domingos de Morais",
-        "Avenida Santo Amaro", "Rua Clodomiro Amazonas", "Rua Tabapuã", "Avenida Hélio Pellegrino", "Avenida Professor Francisco Morato",
-        "Avenida Engenheiro Luiz Carlos Berrini", "Avenida Pedroso de Moraes", "Rua Maria Antônia", "Rua Cardoso de Almeida",
-        "Rua Palestra Itália", "Rua Heitor Penteado", "Rua Lins de Vasconcelos", "Avenida Indianópolis", "Avenida Washington Luís",
-        "Rua Antonio de Barros", "Rua Cantareira", "Avenida Cruzeiro do Sul", "Rua Voluntários da Pátria", "Rua Alfredo Pujol",
-        "Avenida Guapira", "Avenida Cruzeiro do Sul", "Rua Celso Garcia", "Rua Silvio Romero", "Rua do Gasômetro", "Rua Taquari",
-        "Avenida Rudge", "Avenida São João", "Rua Amaral Gurgel", "Rua dos Trilhos", "Avenida Imirim", "Rua Maria Cândida",
-        "Avenida Itaberaba", "Rua Pio XI", "Avenida Professor Abraão de Morais", "Rua Francisco Morato", "Rua Cotoxó",
-        "Rua Cayowaa", "Rua Traipu", "Rua Apinajés", "Rua Paulo Franco", "Avenida Jacu-Pêssego", "Rua Aricanduva",
-        "Avenida Engenheiro Caetano Álvares", "Avenida Marechal Tito", "Rua Padre João Manuel", "Avenida Sumaré", "Rua Pompeu Loureiro",
-        "Rua Maranhão", "Avenida Diógenes Ribeiro de Lima", "Rua Pascoal Ranieri Mazzilli", "Rua Ministro Godói", "Avenida Pompeia",
-        "Rua Madre Cabrini", "Avenida República do Líbano", "Rua Harmonia", "Rua Girassol", "Rua Henrique Schaumann",
-        "Avenida Tiradentes", "Avenida Braz Leme", "Rua Conselheiro Brotero", "Rua Itapura", "Rua Jaboticabal", "Rua Lucrécia Maciel",
-        "Avenida Dr. Ricardo Jafet", "Rua Alvarenga", "Avenida Corifeu de Azevedo Marques", "Avenida Nossa Senhora do Sabará",
-        "Rua Olívia Guedes Penteado", "Rua Alagoas", "Rua Capote Valente", "Rua Doutor Homem de Mello", "Rua Diana",
-        "Rua Votupoca", "Avenida das Nações Unidas", "Rua Pedroso Alvarenga", "Rua Leopoldo Couto de Magalhães",
-        "Rua João Cachoeira", "Rua Vinte e Cinco de Março", "Rua do Carmo", "Rua Piratininga", "Rua Alfredo Maia",
-        "Rua Humberto I", "Rua José Maria Lisboa", "Rua Dr. Rafael de Barros", "Rua Rafael de Barros"
+    # Dicionários de endereços por estado
+    enderecos_por_estado = {
+        "RJ": [
+            "Avenida Atlântica, 1702", "Rua Visconde de Pirajá, 142", "Avenida Ayrton Senna, 3000",
+            "Rua das Laranjeiras, 300", "Rua Conde de Bonfim, 500", "Rua General Glicério, 200",
+            "Rua Marquês de São Vicente, 100", "Rua Voluntários da Pátria, 300", "Rua Jardim Botânico, 700",
+            "Rua Dias Ferreira, 200", "Avenida Vieira Souto, 400", "Rua Barão da Torre, 300",
+            "Rua Santa Clara, 100", "Rua Siqueira Campos, 150", "Rua Tonelero, 200"
+        ],
+        
+        "SP": [
+            "Avenida Paulista, 1000", "Rua Oscar Freire, 800", "Rua Haddock Lobo, 500",
+            "Avenida Brigadeiro Faria Lima, 2000", "Rua Augusta, 1500", "Alameda Santos, 500",
+            "Avenida Rebouças, 1000", "Rua da Consolação, 700", "Rua Bela Cintra, 300",
+            "Avenida Europa, 200", "Rua Estados Unidos, 100", "Rua Joaquim Floriano, 400",
+            "Rua Dr. Melo Alves, 200", "Avenida Angélica, 300", "Rua Pamplona, 500"
+        ],
+        
+        "MG": [
+            "Avenida Getúlio Vargas, 1000", "Rua da Bahia, 500", "Avenida do Contorno, 2000",
+            "Rua Cláudio Manoel, 300", "Rua Antônio de Albuquerque, 400", "Avenida Afonso Pena, 1500",
+            "Rua Tupis, 200", "Rua dos Inconfidentes, 300", "Avenida Cristóvão Colombo, 500",
+            "Rua Professor Moraes, 100", "Rua Alagoas, 200", "Rua Espírito Santo, 300",
+            "Rua Curitiba, 400", "Rua dos Otoni, 500", "Rua Gonçalves Dias, 600"
+        ],
+        
+        "BA": [
+            "Avenida Oceânica, 100", "Rua Portugal, 200", "Avenida Sete de Setembro, 300",
+            "Rua João das Botas, 400", "Rua Marques de Leão, 500", "Avenida Tancredo Neves, 1000",
+            "Rua das Hortênsias, 200", "Rua Patrocínio, 300", "Avenida ACM, 500",
+            "Rua Dias D'Ávila, 400", "Rua do Tijolo, 100", "Rua do Paço, 200",
+            "Rua Chile, 300", "Rua Carlos Gomes, 400", "Rua da Graça, 500"
+        ],
+        
+        "RS": [
+            "Avenida Ipiranga, 1000", "Rua da República, 500", "Avenida João Pessoa, 200",
+            "Rua Gonçalo de Carvalho, 300", "Rua Santana, 400", "Avenida Getúlio Vargas, 500",
+            "Rua Castro Alves, 100", "Rua Ramiro Barcelos, 200", "Avenida Protásio Alves, 300",
+            "Rua João Alfredo, 400", "Rua Venâncio Aires, 500", "Rua Dr. Timóteo, 100",
+            "Rua Lima e Silva, 200", "Rua Sarmento Leite, 300", "Rua Andrade Neves, 400"
+        ],
+        
+        "PR": [
+            "Avenida Batel, 1000", "Rua XV de Novembro, 500", "Avenida Visconde de Guarapuava, 200",
+            "Rua Marechal Deodoro, 300", "Rua Trajano Reis, 400", "Avenida República Argentina, 500",
+            "Rua Brigadeiro Franco, 100", "Rua Desembargador Motta, 200", "Avenida Silva Jardim, 300",
+            "Rua Padre Anchieta, 400", "Rua Comendador Araújo, 500", "Rua 24 de Maio, 100",
+            "Rua São Francisco, 200", "Rua Saldanha Marinho, 300", "Rua Cândido Lopes, 400"
+        ],
+        
+        "PE": [
+            "Avenida Boa Viagem, 1000", "Rua do Riachuelo, 500", "Avenida Conde da Boa Vista, 200",
+            "Rua da Aurora, 300", "Rua do Sol, 400", "Avenida Agamenon Magalhães, 500",
+            "Rua do Hospício, 100", "Rua Gervásio Pires, 200", "Avenida Dantas Barreto, 300",
+            "Rua da Hora, 400", "Rua Sete de Setembro, 500", "Rua do Imperador, 100",
+            "Rua da Matriz, 200", "Rua do Amparo, 300", "Rua da Concórdia, 400"
+        ],
+        
+        "CE": [
+            "Avenida Beira Mar, 1000", "Rua Ildefonso Albano, 500", "Avenida Dom Luís, 200",
+            "Rua Barbosa de Freitas, 300", "Rua José Vilar, 400", "Avenida Santos Dumont, 500",
+            "Rua Carlos Vasconcelos, 100", "Rua Professor Dias da Rocha, 200", "Avenida Washington Soares, 300",
+            "Rua Canuto de Aguiar, 400", "Rua Osório de Paiva, 500", "Rua 24 de Maio, 100",
+            "Rua Guilherme Rocha, 200", "Rua Senador Pompeu, 300", "Rua Pedro I, 400"
+        ],
+        
+        "DF": [
+            "SHIS QI 5 Bloco A", "CLS 104 Bloco C", "SQS 302 Bloco B",
+            "CLN 202 Bloco D", "SCS Qd. 7 Bloco A", "SHIN QI 11 Bloco F",
+            "CLSW 301 Bloco B", "SHS Quadra 3 Bloco A", "CLN 404 Bloco C",
+            "SCES Trecho 2", "SMDB Conjunto 12", "SHDB Conjunto 25",
+            "CLN 106 Bloco B", "SCEN Trecho 2", "SGAN 601 Módulo D"
         ]
-    
+    }
     tipo_sanguineo = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]
 
     estado = random.choice(estados)
     cidade = random.choice(cidades[estado])
-    bairro = random.choice(bairros)
-    endereco = random.choice(enderecos)
+    bairro = random.choice(bairros_por_estado.get(estado, ["Centro"]))  # Default para "Centro" se estado não estiver na lista
+    endereco = random.choice(enderecos_por_estado.get(estado, ["Rua Principal, 100"]))  # Default se estado não estiver na lista
     cep = f"{random.randint(10000, 99999)}-{random.randint(100, 999)}"
 
     return {
